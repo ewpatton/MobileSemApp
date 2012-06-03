882076aa5a30e404b4c9807b7eb4638d4dce4c99
Evan W. Patton <ewpatton@gmail.com>
2012-06-03 14:35:45 -0700

    Sample code for sending data to server

--- android/assets/www/camera.js
+++ android/assets/www/camera.js
@@ -9,8 +9,21 @@ function takePicture() {
 		targetHeight: 256});
 }
 
+function completed(xhr, status) {
+	console.log(status);
+	console.log("HTTP Status: "+xhr.status+" "+xhr.statusText);
+	if(xhr.status>=500) {
+		console.log("Error: "+xhr.responseText);
+	}
+}
+
 function handlePicture(data) {
+	console.log("Camera data received, sending image to server");
 	document.getElementById("image").src="data:image/jpeg;base64,"+data;
+	jQuery.ajax("http://aquarius.tw.rpi.edu/projects/semtech2012/images/test.jpg",
+		{async: true, contentType: "image/jpeg;base64", data:data, 
+		 processData: false, type:"PUT"});
+	console.log(data);
 }
 
 function cameraError(msg) {
