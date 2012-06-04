cbd9aa1be9e9d87a53c699ced393acfb1bff019c
Evan W. Patton <ewpatton@gmail.com>
2012-06-03 22:31:15 -0700

    Added main menu and settings page

--- android/src/edu/rpi/tw/mobile/MobileSemAppActivity.java
+++ android/src/edu/rpi/tw/mobile/MobileSemAppActivity.java
@@ -1,6 +1,8 @@
 package edu.rpi.tw.mobile;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
 import com.phonegap.*;
 
 public class MobileSemAppActivity extends DroidGap {
@@ -8,6 +10,9 @@ public class MobileSemAppActivity extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, 
+                WindowManager.LayoutParams.FLAG_FULLSCREEN | 
+                WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
         super.loadUrl("file:///android_asset/www/index.html");
     }
 }
